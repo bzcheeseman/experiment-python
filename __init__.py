50048df1a87fd9eaf015b95fbc185f2452e85eb3
bzcheeseman <alachapelle@uchicago.edu>
2015-10-22 14:10:56 -0500

    Fixed untitled.py, working for first site

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from basics import get_data, error_handling
+from fit_funcs import *
 
 __all__ = ['basics', 'fit_funcs']
--- fit_funcs/untitled.py
+++ fit_funcs/untitled.py
@@ -11,6 +11,8 @@ def main():
 	tunnelings = {}
 	emmu = {}
 
+	#1,1, 2,2, 3,3, ... 1,2, 2,3, 3,4, 4,5, 
+
 	parms = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
 			1, 1, 1, 0.0306307, 0.0580278, 0.0748212, 0.146737, 0.181921, 0.0281016, 
 			0.034514, 0.200078, 0.0371738, 0.117055, 0.0528912, 0.0380498, 
@@ -25,68 +27,21 @@ def main():
 	meas_energies = parms[-nres:]
 	meas_trans_phi = parms[1+2*nres:-nres]
 
+####find mu 11
 	mu11 = 0
 	for i in range(0, nres):
-		mu11 += meas_energies[i]*(meas_ref_phi[i]**2)
+		mu11 += meas_energies[i]*(np.absolute(meas_ref_phi[i]))
 
 	base_freqs["(1,1)"] = mu11
-	print mu11
+	print base_freqs
 
+####find t_11->12
+	t1112 = 0
+	for i in range(0, nres):
+		t1112 += meas_energies[i] * np.absolute(meas_trans_phi[i]*meas_trans_phi[i+nres])
 
-	'''
-	##find all the parameters
-	for k in range(1, Ny+1): #indexing (?) - might cause an error
-		##find parameters row by row in -|-|-|...-| shape
-		for i in range(1, Nx+1):
-			##define popt
-
-			popt = parms
-
-			phi = popt[1+i*nres:1+i*nres+12]
-
-			##find the mu
-			mu = 0
-			for a in range(nres, 0, -1):
-				for l in range(0, nres):
-					mu += (popt[-a]*np.absolute(phi[l]**2))
-
-			print mu
-		
-			base_freqs["(%d,%d)" % (k,i)] = mu
-			emmu["(%d,%d)" % (k,i)] = np.subtract(popt[-nres:], mu)
-			phis["(%d,%d)" % (k,i)] = phi
-
-			##now we have the phi and mu for (1, i+1), need to find t_(1,1)_(1,i+1)
-
-			try: ##in case we're on the edge (i,j) = (1,j)
-				t = 0
-				for j in range(1, nres):
-					t += (popt[j+2*nres]*phis["(%d,%d)" % (k,i-1)][j-1]\
-						*phis["(%d,%d)" % (k,i)][j-1])
-
-				tunnelings["(%d,%d)-(%d,%d)" % (k,i-1, k,i)] = t
-
-				##so now we have the mu/t/phi for the first row (hopefully)
-					##time to find the vertical tunnelings
-
-				try: ##in case we're on the edge (i,j) = (i, Ny)
-					t = 0
-					t = -np.sqrt(np.sum( (emmu["(%d,%d)" % (k,i-1)]*phis["(%d,%d)" % (k,i)] \
-						- np.multiply(tunnelings["(%d,%d)-(%d,%d)" % (k,i-1, k,i)],phis["(%d,%d)" % (k,i)])**2 )))
-					tunnelings["(%d,%d)-(%d,%d)" % (k+1,i, k,i)] = t
-
-				except KeyError:
-					tunnelings["(%d,%d)-(%d,%d)" % (k+1, i)] = 0
-
-			except KeyError:
-				tunnelings["(%d,%d)-(%d,%d)" % (k,i-1, k,i)] = 0
-
-			##aaaaand carriage return, next row, same pattern
-
-			##hopefully all the extraneous tunnelings (to the left and below) will be zero
-				##and will be easily filtered out
-
-	return {"base_freqs":base_freqs, "phis":phis, "tunnelings":tunnelings}'''
+	tunnelings["(1,1)-(1,2)"] = t1112
+	print tunnelings
 
 if __name__ == '__main__':
 	main()
